13278da92afd3397bc21cf12516dc72f88c9db49
EmadHammoudeEJ <emad.hammoude@camb.ai>
2025-11-24 05:09:13 +0400

    Finished script (untested). No docker installation added yet

--- 42ool.sh
+++ 42ool.sh
@@ -6,6 +6,7 @@ mkdir ~/42ool && cd ~/42ool
 git clone https://github.com/ombhd/Cleaner_42.git 42Cleaner || echo "Unexpected issue while fetching cclean tool"
 $(cd 42Cleaner && chmod +x ./CleanerInstaller.sh && yes | ./CleanerInstaller.sh) || echo "Unexpected issue while running cclean tool"
 bash -c "$(curl -fsSL https://raw.github.com/xicodomingues/francinette/master/bin/install.sh)" || echo "Unexpected issue while fetching francinette tool"
+$(git clone https://github.com/zakarm/Valgrind_42_Network.git && cd Valgrind_42_Network  && chmod +x script.sh && ./script.sh) || echo "Unexpected error while installing valgrind"
 should_add=$(cat ~/.zshrc | grep "ZAX" | wc -w )
 if [[ "$should_add" -eq 0 ]]; then
 	echo "Creating a folder for your projects on Desktop. Please enter folder name:"
@@ -13,9 +14,6 @@ if [[ "$should_add" -eq 0 ]]; then
 	proj_folder="$HOME/Desktop/$proj_folder"
 	echo "
 	# Helpful shortcuts brought to you by ZAX
-	42() {
-		cd $proj_folder
-	}
 	makemake() {
 		echo '
 			CC=cc
@@ -43,17 +41,24 @@ if [[ "$should_add" -eq 0 ]]; then
 			.PHONY: all clean fclean re'
 		>Makefile && echo "Default Makefile created"
 	}
+	42() {
+		cd $proj_folder
+	}
+	go() {
+		cd "$proj_folder/\$1"
+	}
 	project() {
-		mkdir -p "$proj_folder/$1" && cd "$proj_folder/$1" && makemake
+		mkdir -p "$proj_folder/\$1" && go "\$1" && makemake
 	}
 	connect() {
-		
+		git config --global init.defaultBranch main;
+		go "\$1" && rm -rf .git && git init && git add . && git commit -m "First project commit" && git branch -M main && git remote add origin "\$2" && git push -u origin main
 	}
 	re() {
 		make re && make clean
 	}
 	push() {
-		git add . && git commit -m "$1" && git push
+		git add . && git commit -m "\$1" && git push
 	}
 	" >>~/.zshrc
 fi
